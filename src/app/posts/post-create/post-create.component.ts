07116d6e4ce2571ebc14d5a117395d407850aeaa
travis.vela <tvela@appddictionstudio.com>
2020-04-12 03:30:41 -0500

    add post model and event emitters to capture data from mat-form-fields

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddPost() {
+    const post: Post ={
+      title: this.enteredTitle,
+      content: this.enteredContent
+    }
+    console.log(post);
+    this.postCreated.emit(post);
+  }
+
+}
